db34357b523865030b43fb603fb81a846d9dc55b
HashirBaig <hashirbaig456@gmail.com>
2026-09-26 17:14:09 +0200

    minor adjustments

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -102,203 +102,214 @@ function LandingPage() {
     <Wrapper>
       <Hero reset={resetGame} />
 
-      {/* Match Card */}
-      <div className="bg-green-50/90 w-full rounded-xl mt-8 p-5">
-        <div className="flex items-center justify-center">
-          <img src={PlayersLogo} alt="Player logo" className="w-40 h-auto" />
+      <div className="sm:flex sm:gap-8">
+        {/* Match Card */}
+        <div className="bg-green-50/90 w-full rounded-xl mt-8 p-5 sm:flex sm:flex-col sm:grow">
+          <div className="flex items-center justify-center">
+            <img src={PlayersLogo} alt="Player logo" className="w-40 h-auto" />
+          </div>
+
+          <form className="mt-8 space-y-4" onSubmit={handleOnSubmit}>
+            <div className="space-y-2">
+              <Label
+                htmlFor="player-1"
+                className="text-green-950 font-semibold text-md"
+              >
+                Player 1
+              </Label>
+              <Input
+                id="player-1"
+                type="text"
+                placeholder="e.g John Doe"
+                value={formData.playerOne}
+                onChange={(e) =>
+                  setFormData({ ...formData, playerOne: e?.target?.value })
+                }
+                disabled={isLoading}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label
+                htmlFor="player-2"
+                className="text-green-950 font-semibold text-md"
+              >
+                Player 2
+              </Label>
+              <Input
+                id="player-2"
+                type="text"
+                placeholder="e.g Doe John"
+                value={formData.playerTwo}
+                onChange={(e) =>
+                  setFormData({ ...formData, playerTwo: e?.target?.value })
+                }
+                disabled={isLoading}
+              />
+            </div>
+
+            {/* Game Card */}
+            {formData?.playerOne && formData?.playerTwo && (
+              <div className="w-full mt-8 bg-green-800 rounded-xl space-y-6 p-3">
+                <h1 className="text-center text-green-50 font-extrabold">
+                  {`${formData?.playerOne} VS ${formData?.playerTwo}`}
+                </h1>
+
+                <div className="space-y-2">
+                  <h1 className="text-green-50 text-center font-normal text-sm">
+                    Sets
+                  </h1>
+
+                  <div className="flex items-center justify-center gap-3">
+                    <Button
+                      variant={"circle"}
+                      onClick={() => handleSet({ incr: false })}
+                    >
+                      <Minus className="size-7" />
+                    </Button>
+
+                    <span className="text-green-950 bg-green-50/90 w-36 h-18 flex items-center justify-center text-3xl font-semibold rounded-2xl">
+                      {setCounter}
+                    </span>
+
+                    <Button
+                      variant={"circle"}
+                      onClick={() => handleSet({ incr: true })}
+                    >
+                      <Plus className="size-7" />
+                    </Button>
+                  </div>
+                </div>
+
+                <div className="space-y-2">
+                  <h1 className="text-green-50 text-center font-normal text-sm">
+                    Games
+                  </h1>
+
+                  <div className="flex items-center justify-center gap-3">
+                    <Button
+                      variant={"circle"}
+                      onClick={() => handleGame({ incr: false })}
+                    >
+                      <Minus className="size-7" />
+                    </Button>
+
+                    <span className="text-green-950 bg-green-50/90 w-36 h-18 flex items-center justify-center text-3xl font-semibold rounded-2xl">
+                      {gameCounter}
+                    </span>
+
+                    <Button
+                      variant={"circle"}
+                      onClick={() => handleGame({ incr: true })}
+                    >
+                      <Plus className="size-7" />
+                    </Button>
+                  </div>
+                </div>
+
+                <div className=" border-b border-yellow-50/50"></div>
+
+                <div className="flex items-center justify-around">
+                  <div className="space-y-2">
+                    <h1 className="text-green-50 font-semibold text-center text-sm">
+                      {formData?.playerOne}
+                    </h1>
+
+                    <div className="flex flex-col items-center gap-2">
+                      <Button
+                        variant={"circle"}
+                        className={"w-fit"}
+                        onClick={() =>
+                          handleGame({ incr: false, player: "one" })
+                        }
+                      >
+                        <Minus className="size-7" />
+                      </Button>
+
+                      <span className="text-green-950 bg-green-50/90 w-16 h-16 flex items-center justify-center text-3xl font-semibold rounded-2xl">
+                        {player1Games}
+                      </span>
+
+                      <Button
+                        variant={"circle"}
+                        className={"w-fit"}
+                        onClick={() =>
+                          handleGame({ incr: true, player: "one" })
+                        }
+                      >
+                        <Plus className="size-7" />
+                      </Button>
+                    </div>
+                  </div>
+
+                  <div className="space-y-2">
+                    <h1 className="text-green-50 font-semibold text-center text-sm">
+                      {formData?.playerTwo}
+                    </h1>
+
+                    <div className="flex flex-col items-center gap-2">
+                      <Button
+                        variant={"circle"}
+                        className={"w-fit"}
+                        onClick={() =>
+                          handleGame({ incr: false, player: "two" })
+                        }
+                      >
+                        <Minus className="size-7" />
+                      </Button>
+
+                      <span className="text-green-950 bg-green-50/90 w-16 h-16 flex items-center justify-center text-3xl font-semibold rounded-2xl">
+                        {player2Games}
+                      </span>
+
+                      <Button
+                        variant={"circle"}
+                        className={"w-fit"}
+                        onClick={() =>
+                          handleGame({ incr: true, player: "two" })
+                        }
+                      >
+                        <Plus className="size-7" />
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className="flex items-center justify-center">
+              <Button type="submit">
+                <img src={RacketLogo} alt="ball logo" className="w-10 h-auto" />
+                <span>Save Game</span>
+              </Button>
+            </div>
+          </form>
         </div>
 
-        <form className="mt-8 space-y-4" onSubmit={handleOnSubmit}>
-          <div className="space-y-2">
-            <Label
-              htmlFor="player-1"
-              className="text-green-950 font-semibold text-md"
-            >
-              Player 1
-            </Label>
-            <Input
-              id="player-1"
-              type="text"
-              placeholder="e.g John Doe"
-              value={formData.playerOne}
-              onChange={(e) =>
-                setFormData({ ...formData, playerOne: e?.target?.value })
-              }
-              disabled={isLoading}
-            />
-          </div>
+        {/* Match History */}
+        <div className="bg-green-50/90 w-full rounded-xl mt-8 p-5 sm:flex sm:flex-col sm:grow sm:h-fit">
+          <h1 className="text-green-950 text-2xl font-semibold text-center sm:text-start">
+            Match History
+          </h1>
 
-          <div className="space-y-2">
-            <Label
-              htmlFor="player-2"
-              className="text-green-950 font-semibold text-md"
-            >
-              Player 2
-            </Label>
-            <Input
-              id="player-2"
-              type="text"
-              placeholder="e.g Doe John"
-              value={formData.playerTwo}
-              onChange={(e) =>
-                setFormData({ ...formData, playerTwo: e?.target?.value })
-              }
-              disabled={isLoading}
-            />
-          </div>
-
-          {/* Game Card */}
-          {formData?.playerOne && formData?.playerTwo && (
-            <div className="w-full mt-8 bg-green-800 rounded-xl space-y-6 p-3">
-              <h1 className="text-center text-green-50 font-extrabold">
-                {`${formData?.playerOne} VS ${formData?.playerTwo}`}
-              </h1>
-
-              <div className="space-y-2">
-                <h1 className="text-green-50 text-center font-normal text-sm">
-                  Sets
-                </h1>
-
-                <div className="flex items-center justify-center gap-3">
-                  <Button
-                    variant={"circle"}
-                    onClick={() => handleSet({ incr: false })}
-                  >
-                    <Minus className="size-7" />
-                  </Button>
-
-                  <span className="text-green-950 bg-green-50/90 w-36 h-18 flex items-center justify-center text-3xl font-semibold rounded-2xl">
-                    {setCounter}
-                  </span>
-
-                  <Button
-                    variant={"circle"}
-                    onClick={() => handleSet({ incr: true })}
-                  >
-                    <Plus className="size-7" />
-                  </Button>
-                </div>
-              </div>
-
-              <div className="space-y-2">
-                <h1 className="text-green-50 text-center font-normal text-sm">
-                  Games
-                </h1>
-
-                <div className="flex items-center justify-center gap-3">
-                  <Button
-                    variant={"circle"}
-                    onClick={() => handleGame({ incr: false })}
-                  >
-                    <Minus className="size-7" />
-                  </Button>
-
-                  <span className="text-green-950 bg-green-50/90 w-36 h-18 flex items-center justify-center text-3xl font-semibold rounded-2xl">
-                    {gameCounter}
-                  </span>
-
-                  <Button
-                    variant={"circle"}
-                    onClick={() => handleGame({ incr: true })}
-                  >
-                    <Plus className="size-7" />
-                  </Button>
-                </div>
-              </div>
-
-              <div className=" border-b border-yellow-50/50"></div>
-
-              <div className="flex items-center justify-around">
-                <div className="space-y-2">
-                  <h1 className="text-green-50 font-semibold text-center text-sm">
-                    {formData?.playerOne}
-                  </h1>
-
-                  <div className="flex flex-col items-center gap-2">
-                    <Button
-                      variant={"circle"}
-                      className={"w-fit"}
-                      onClick={() => handleGame({ incr: false, player: "one" })}
-                    >
-                      <Minus className="size-7" />
-                    </Button>
-
-                    <span className="text-green-950 bg-green-50/90 w-16 h-16 flex items-center justify-center text-3xl font-semibold rounded-2xl">
-                      {player1Games}
-                    </span>
-
-                    <Button
-                      variant={"circle"}
-                      className={"w-fit"}
-                      onClick={() => handleGame({ incr: true, player: "one" })}
-                    >
-                      <Plus className="size-7" />
-                    </Button>
-                  </div>
-                </div>
-
-                <div className="space-y-2">
-                  <h1 className="text-green-50 font-semibold text-center text-sm">
-                    {formData?.playerTwo}
-                  </h1>
-
-                  <div className="flex flex-col items-center gap-2">
-                    <Button
-                      variant={"circle"}
-                      className={"w-fit"}
-                      onClick={() => handleGame({ incr: false, player: "two" })}
-                    >
-                      <Minus className="size-7" />
-                    </Button>
-
-                    <span className="text-green-950 bg-green-50/90 w-16 h-16 flex items-center justify-center text-3xl font-semibold rounded-2xl">
-                      {player2Games}
-                    </span>
-
-                    <Button
-                      variant={"circle"}
-                      className={"w-fit"}
-                      onClick={() => handleGame({ incr: true, player: "two" })}
-                    >
-                      <Plus className="size-7" />
-                    </Button>
-                  </div>
-                </div>
-              </div>
+          {/* Match Card */}
+          <div className="w-full bg-green-800 p-4 rounded-2xl mt-8 space-y-2">
+            <div className="flex items-center justify-between">
+              <span className="text-green-50 font-normal text-center w-fit">
+                Sinner vs Alcaraz
+              </span>
+              <Badge className="bg-gray-50/30 text-gray-50 border-gray-50/20 font-semibold">
+                2 - 1
+              </Badge>
             </div>
-          )}
 
-          <div className="flex items-center justify-center">
-            <Button type="submit">
-              <img src={RacketLogo} alt="ball logo" className="w-10 h-auto" />
-              <span>Save Game</span>
-            </Button>
-          </div>
-        </form>
-      </div>
-
-      <div className="bg-green-50/90 w-full rounded-xl mt-8 p-5">
-        <h1 className="text-green-950 text-2xl font-semibold text-center sm:text-start">
-          Match History
-        </h1>
-
-        {/* Match Card */}
-        <div className="w-full bg-green-800 p-4 rounded-2xl mt-8 space-y-2">
-          <div className="flex items-center justify-between">
-            <span className="text-green-50 font-normal text-center w-fit">
-              Sinner vs Alcaraz
-            </span>
-            <Badge className="bg-gray-50/30 text-gray-50 border-gray-50/20 font-semibold">
-              2 - 1
-            </Badge>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <span className="text-sm font-semibold">26/9/20206</span>
-            <Badge className="bg-yellow-500/30 text-yellow-500 border-yellow-500/20 font-semibold">
-              <Crown className="size-4" />
-              Sinner
-            </Badge>
+            <div className="flex items-center justify-between">
+              <span className="text-sm font-semibold">26/9/20206</span>
+              <Badge className="bg-yellow-500/30 text-yellow-500 border-yellow-500/20 font-semibold">
+                <Crown className="size-4" />
+                Sinner
+              </Badge>
+            </div>
           </div>
         </div>
       </div>
